42991274b5f6c7697077f9e476ab4a89d2aaf4bd
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-03-05 09:34:44 +0100

    Fixed resultant forces' equations

--- suddenBurst.m
+++ suddenBurst.m
@@ -6,13 +6,13 @@ if SB==1
     m=W_T/9.81;
     D=D*1.1;
     L=L*1.1;
-    Faz=W-L;    % Resultant z force. 
+    Faz=L-W_T;    % Resultant z force. 
     % Solving Fax resultant force. T' doesn't generate moment due to the
     % selected point of application
     syms Fax    
     eqn = D*H+Fax*z_cg+W_T*x_cg == (7/5)*L*W+Faz*x_cg;
     Fax = vpasolve(eqn,Fax);
-    T=D+Fax;
+    T=D-Fax;
 end
 
 %SB is an auxiliar variable whose function if to apply the sudden burst or
