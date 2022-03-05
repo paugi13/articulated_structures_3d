946670757d9227aaf379a72cce3e7c02994c3b5e
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-03-05 10:40:37 +0100

    Considered intern accelerations
    The momentum equation was changed in order not to include CG coordinates.

--- suddenBurst.m
+++ suddenBurst.m
@@ -1,4 +1,4 @@
-function [T,D,L,Fax,Faz] = suddenBurst(SB,x_cg,z_cg,D,L,W_T,W,H,T)
+function [T,D,L,Fax,Faz] = suddenBurst(SB,x_cg,z_cg,D,L,W_T,W,H,T, W_C, W_D)
 %UNTITLED Summary of this function goes here
 %   Les posicions dels nodes es referencien respecte el pla de simetria de
 %   l'ala delta i respecte els punts 1 i 2
@@ -9,9 +9,12 @@ if SB==1
     Faz=L-W_T;    % Resultant z force. 
     % Solving Fax resultant force. T' doesn't generate moment due to the
     % selected point of application
+    % Resultant forces are considered intern, so they point to the opposite
+    % direction, wich is the exterior and the one we would see. 
     syms Fax    
-    eqn = D*H+Fax*z_cg+W_T*x_cg == (7/5)*L*W+Faz*x_cg;
+    eqn = D*H - 5*Fax/7*H + W_C*W + W_D*2*W == (7/5)*L*W - 6*Faz*W/7 - Faz*W/7;
     Fax = vpasolve(eqn,Fax);
+%   Fax = ((7/5)*L*W+Faz*x_cg - W_T*x_cg - D*H)/z_cg;
     T=D-Fax;
 end
 
