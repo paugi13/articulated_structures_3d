72856e1ee4114f2e7388ae9c14427e893c3b7e2a
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-09-01 19:03:26 +0200

    Removed initializing method
    Now main geometrical values are inherited

--- articulated3Dproblem.m
+++ articulated3Dproblem.m
@@ -3,6 +3,7 @@ classdef articulated3Dproblem < problemDef
     %   Detailed explanation goes here
     
     properties (Access = private)
+        % Properties related to problem resolution
         KG
         K_e
         
@@ -13,12 +14,16 @@ classdef articulated3Dproblem < problemDef
         
         eps
         sig
+        
+        vL
+        vR
+        uR
     end
     
     methods (Access = public)
-        function obj = initializeProblem(objPD)
-            obj.initialize(objPD);
-        end
+%         function obj = articulated3Dproblem(objPD)
+%             obj.initialize(objPD);
+%         end
         
         function solver(obj)
             obj.assembleKG();
@@ -41,33 +46,36 @@ classdef articulated3Dproblem < problemDef
         
         function computeResults(obj)
             [obj.eps,obj.sig] = computeStrainStressBar(obj.n_d,...
-                obj.n_el,obj.u_method,obj.Td,obj.x,obj.Tnod,mat,Tmat);
+                obj.n_el,obj.u_method,obj.Td,obj.x,obj.Tnod,obj.mat,obj.Tmat);
         end
+            
+        % Not possible because these properties could only be modified
+        % through specific methods and now they are defined as constant for
+        % all inheritant classes. 
+%         function initialize(obj, objPD)
+%             obj.H = objPD.H;
+%             obj.W = objPD.W;
+%             obj.B = objPD.B;
+%             obj.D1 = objPD.D1;
+%             obj.d1 = objPD.d1;
+%             obj.D2 = objPD.D2;
+%             obj.M = objPD.M;
+%             obj.x = objPD.x;
+%             obj.Tnod = objPD.Tnod;
+%             obj.fixNod = objPD.fixNod;
+%             obj.n_d = objPD.n_d;
+%             obj.n = objPD.n;
+%             obj.n_i = objPD.n_i;
+%             obj.n_dof = objPD.n_dof;
+%             obj.n_el = objPD.n_el;
+%             obj.n_nod = objPD.n_nod;
+%             obj.n_el_dof = objPD.n_el_dof;
+%             obj.Td = objPD.Td;
+%             obj.mat = objPD.mat;
+%             obj.Tmat = objPD.Tmat;
+%         end
         
-        function initialize(obj, objPD)
-            obj.H = objPD.H;
-            obj.W = objPD.W;
-            obj.B = objPD.B;
-            obj.D1 = objPD.D1;
-            obj.d1 = objPD.d1;
-            obj.D2 = objPD.D2;
-            obj.M = objPD.M;
-            obj.x = objPD.x;
-            obj.Tnod = objPD.Tnod;
-            obj.fixNod = objPD.fixNod;
-            obj.n_d = objPD.n_d;
-            obj.n = objPD.n;
-            obj.n_i = objPD.n_i;
-            obj.n_dof = objPD.n_dof;
-            obj.n_el = objPD.n_el;
-            obj.n_nod = objPD.n_nod;
-            obj.n_el_dof = objPD.n_el_dof;
-            obj.Td = objPD.Td;
-            obj.mat = objPD.mat;
-            obj.Tmat = objPD.Tmat;
-        end
-        
-        function asssembleKG(obj)
+        function assembleKG(obj)
             obj.K_e = computeKelBar(obj.n_d,obj.n_el,obj.x,obj.Tnod,obj.mat...
                 ,obj.Tmat);
             obj.KG = assemblyKG(obj.n_el,obj.n_el_dof,obj.n_dof,obj.Td,...
@@ -75,16 +83,16 @@ classdef articulated3Dproblem < problemDef
         end
         
         function applyConditions(obj)
-            [obj.vL,obj.vR,obj.uR] = applyCond(n_i,n_dof,fixNod);
+            [obj.vL,obj.vR,obj.uR] = applyCond(obj.n_i,obj.n_dof,obj.fixNod);
         end
         
         function computeF(obj)
-            F_bar = density_calc(x,mat, Tmat, obj.n_el, obj.Td, obj.Tnod);
+            F_bar = density_calc(obj.x,obj.mat, obj.Tmat, obj.n_el, obj.Td, obj.Tnod);
             % this restricts it to the mentioned geometry. Won't solve it
             % for any structure. 
             [T,L,D,~,~,~] =  equilibrio_momentos(F_bar,...
-                W_M,obj.H,obj.W);
-            Fdata = computeFdata(W_M, L, D, T); 
+                obj.W_M,obj.H,obj.W);
+            Fdata = computeFdata(obj.W_M, L, D, T); 
             obj.Fext = computeF(obj.n_i,obj.n_dof, Fdata, F_bar);
         end
         
@@ -94,7 +102,6 @@ classdef articulated3Dproblem < problemDef
         end
             
     end
-    
-    
+  
 end
 
