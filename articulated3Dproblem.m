94a2ac62cce90821c9962d573d120a4b25e6cd10
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-09-03 13:25:16 +0200

    Fixed problem initialization
    Now the problem is initialized through the main script. Changing the problem geometry is now easier as it is only necessary to load another .mat file

--- articulated3Dproblem.m
+++ articulated3Dproblem.m
@@ -1,6 +1,5 @@
-classdef articulated3Dproblem < problemDef
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+classdef Articulated3Dproblem < handle
+    % Class that directly solves the problem
     
     properties (Access = protected)
         % Properties related to problem resolution
@@ -18,14 +17,20 @@ classdef articulated3Dproblem < problemDef
         vL
         vR
         uR
+        Td
+    end
+    
+    properties (Access = private)
+        var_struct
     end
     
     methods (Access = public)
-%         function obj = articulated3Dproblem(objPD)
-%             obj.initialize(objPD);
-%         end
+        function obj = Articulated3Dproblem(cParams)
+            obj.init(cParams);
+        end
         
         function solver(obj)
+            obj.connectDofs();
             obj.assembleKG();
             obj.applyConditions();
             obj.computeF();
@@ -39,23 +44,38 @@ classdef articulated3Dproblem < problemDef
     end
     
     methods (Access = private)
+        function init(obj, cParams)
+            obj.var_struct = cParams;
+        end
+        
         function solveSystem(obj)
             [obj.u_method,obj.R] = solveSys(obj.vL,obj.vR,obj.uR,...
                 obj.KG,obj.Fext);
         end
         
         function computeResults(obj)
-            [obj.eps,obj.sig] = computeStrainStressBar(obj.n_d,...
-                obj.n_el,obj.u_method,obj.Td,obj.x,obj.Tnod,obj.mat,obj.Tmat);
+            [obj.eps,obj.sig] = computeStrainStressBar(obj.var_struct.n_d,...
+                obj.var_struct.n_el,obj.u_method,obj.Td,obj.var_struct.x,...
+                obj.var_struct.Tnod,obj.var_struct.mat,obj.var_struct.Tmat);
+        end
+        
+        function connectDofs(obj)
+            s.n_el = obj.var_struct.n_el;
+            s.n_nod = obj.var_struct.n_nod;
+            s.n_i = obj.var_struct.n_i;
+            s.Tnod = obj.var_struct.Tnod;
+            dofConnection = DofsMatrixAssembler(s);
+            dofConnection.assembleTd();
+            obj.Td = dofConnection.Td;
         end
         
         function assembleKG(obj)
-            s.n_d = obj.n_d;
-            s.n_el = obj.n_el;
-            s.x = obj.x;
-            s.Tnod = obj.Tnod;
-            s.mat = obj.mat;
-            s.Tmat = obj.Tmat;
+            s.n_d = obj.var_struct.n_d;
+            s.n_el = obj.var_struct.n_el;
+            s.x = obj.var_struct.x;
+            s.Tnod = obj.var_struct.Tnod;
+            s.mat = obj.var_struct.mat;
+            s.Tmat = obj.var_struct.Tmat;
             
             % Stiffness matrix for every element
             ke_comp = ElementalStiffnessMat(s);
@@ -63,9 +83,9 @@ classdef articulated3Dproblem < problemDef
             obj.K_e = ke_comp.K_e;
             
             % Global stiffness matrix assembly
-            s.n_el_dof = obj.n_el_dof;
-            s.n_dof    = obj.n_dof;
-            s.n_el     = obj.n_el;
+            s.n_el_dof = obj.var_struct.n_el_dof;
+            s.n_dof    = obj.var_struct.n_dof;
+            s.n_el     = obj.var_struct.n_el;
             s.K_e      = obj.K_e;
             s.Td       = obj.Td;
             
@@ -75,21 +95,21 @@ classdef articulated3Dproblem < problemDef
         end
         
         function applyConditions(obj)
-            [obj.vL,obj.vR,obj.uR] = applyCond(obj.n_i,obj.n_dof,obj.fixNod);
+            [obj.vL,obj.vR,obj.uR] = applyCond(obj.var_struct.n_i,obj.var_struct.n_dof,obj.var_struct.fixNod);
         end
         
         function computeF(obj)
-            s.x    = obj.x;
-            s.mat  = obj.mat;
-            s.Tmat = obj.Tmat;
-            s.n_el = obj.n_el;
+            s.x    = obj.var_struct.x;
+            s.mat  = obj.var_struct.mat;
+            s.Tmat = obj.var_struct.Tmat;
+            s.n_el = obj.var_struct.n_el;
             s.Td   = obj.Td;
-            s.Tnod = obj.Tnod;
-            s.W_M = obj.W_M;
-            s.H = obj.H;
-            s.W = obj.W;
-            s.n_i = obj.n_i;
-            s.n_dof = obj.n_dof;
+            s.Tnod = obj.var_struct.Tnod;
+            s.W_M = obj.var_struct.W_M;
+            s.H = obj.var_struct.H;
+            s.W = obj.var_struct.W;
+            s.n_i = obj.var_struct.n_i;
+            s.n_dof = obj.var_struct.n_dof;
             
             weights_calculus = DensityWeightCalculator(s);
             weights_calculus.calculateWeights();
@@ -119,10 +139,10 @@ classdef articulated3Dproblem < problemDef
         end
         
         function plot3D(obj) 
-            s.Tnod = obj.Tnod;
+            s.Tnod = obj.var_struct.Tnod;
             s.u = obj.u_method;
             s.sig = obj.sig;
-            s.x = obj.x;
+            s.x = obj.var_struct.x;
             setPlot = PlotterClass(s);
             setPlot.plotResults();
         end
